b8a6d0ceb9715c62c1f63d23f035aec872191ca3
jtherrera1 <jthvasago@gmail.com>
2017-07-20 00:50:12 -0300

    Update README

--- module/data_rdf_wd.lua
+++ module/data_rdf_wd.lua
@@ -3,10 +3,9 @@ local redis = require 'redis_config'
 local url_web = require"socket.url"
 local local_data = require"mysql_data"
 local entity_class = require"abstraction_entity_class"
-local relevance = require"relevance_metric"
 
 local M = {
-        _COPYRIGHT = "Copyright (C) 2009-2017 PUC-Rio",
+        _COPYRIGHT = "Copyright (C) 2017 PUC-Rio",
 	start_node = nil,
 	end_node = nil,
         property_ignore=nil,
@@ -15,8 +14,8 @@ local M = {
         DISTANCE = 4,	
 }
 
-local client6379 =nil 
-local client6380 =nil 
+local client6379 =nil
+local client6380 =nil
 
 local start = nil
 local direction = nil
@@ -28,14 +27,30 @@ local database =nil
 local nivel = nil
 local run_start = false
 
+local mapping_class={
+"artist",
+"movie",
+"genre",
+"television",
+"actor",
+"cinematographer",
+"director",
+"dubbing_performance",
+"editor",
+"film",
+"musiccontributor",
+"producer",
+"production",
+"writer"
+ }
 
-function M.init(seq,niv,act,db, node, direc, cli6379, cli6380,  node_start, node_end, run_start_)
+function M.init(seq,niv,act,db, node, direc, cli6379, cli6380, node_start, node_end, run_start_)
 	sequence = seq
 	action = act
 	database = db
 	nivel = niv
 
-	if node then  
+	if node then 
 		start = node
 		direction = direc
 	end
@@ -43,8 +58,9 @@ function M.init(seq,niv,act,db, node, direc, cli6379, cli6380,  node_start, node
 	client6380 = cli6380
 
 	run_start = run_start_
+		
+        local_data.client6380 = cli6380
 end
-
 
 function M.getCodeUrl(url)
 	local code =local_data.getCodeUrl(url)
@@ -67,13 +83,15 @@ local function dbpedia_is(url,is_property, ntype)
 									code = local_data.getCodeUrlProperty(url)
 									code_set = code
 									ntype =  tonumber(ntype)		
-                                                                        code = (ntype>2) and code.."@" or code 
+                                                                        code = (ntype>2) and code.."@" or code --get code properties in path
 								else
 									code =local_data.getCodeUrl(url)
 									code_set = code	
                                                                 end
                                                                 client6380:hsetnx("decode_url",code_set,url)
+
                                                                 return true, code
+								-------------------------------------------------------		
                                                         else
                                                                 return false, nil
 
@@ -87,8 +105,9 @@ local function dbpedia_is(url,is_property, ntype)
            end
 
 end
-local filter={
-		is= {
+local filter=
+{
+		is= { 
 			wikidata= function(url)
 			        if url and (string.match(url, "^Q") ) then
 					return true
@@ -96,7 +115,7 @@ local filter={
 					return false
 				end
 			 end,
-			 dbpedia = function(url,is_property, ntype) --ntype inverse
+			 dbpedia = function(url,is_property, ntype)
 				  return dbpedia_is(url,is_property, ntype)			
 			 end
 		},
@@ -114,17 +133,18 @@ local filter={
 }
 
 local function split(string)
-	local parts ={}
-	local str =""
-	local temp ={}
-	for word in string.gmatch(string, "%S+") do
-		if not temp [word] then
-	       		temp [word] = ""
-			table.insert(parts,word)
-	       		str = word .." ".. str .. " "
-		end
+local parts ={}
+local str =""
+local temp ={}
+for word in string.gmatch(string, "%S+") do
+	if not temp [word] then
+	       temp [word] = ""
+		table.insert(parts,word)
+	       str = word .." ".. str .. " "
 	end
-	return str,parts
+end
+return str,parts
+
 end
 
 
@@ -187,27 +207,27 @@ local function load_description (desc)
 end
 local function get_data_description(url,code)
 
-     local description = client6380:hget(url,"description")
+    local description = client6380:hget(url,"description")
 
-     if not description and code then
+    if not description then
               description = local_data.get_description(code) --code
-     end
+    end
 
     local entity_property = nil
-     if not description then
+    if not description then
 
             local temp_url = url:gsub("http://dbpedia.org/resource/","")
             local n3_url =  "http://dbpedia.org/page/"..temp_url
+
             temp_url = "http://dbpedia.org/page/"..url_web.escape(temp_url)
             entity_property = xmlp.getStructure("dbpedia",temp_url, n3_url)
             description=""
 	    if entity_property then
-            for property, values in pairs(entity_property) do
-                local tmp_desc = xmlp.callbacks.serializedb[database](values,property)
-                description = description..tmp_desc..","
-            end
+        	    for property, values in pairs(entity_property) do
+                	local tmp_desc = xmlp.callbacks.serializedb[database](values,property)
+                	description = description..tmp_desc..","
+            	    end
 	    end
-	
         else
 
 	    local  status, e = pcall(load_description, description)
@@ -215,10 +235,8 @@ local function get_data_description(url,code)
 	      return nil, nil
 	    end
             entity_property =  e 
-
-     end
+    end
     if description and  #description >0 then
-
             return description, entity_property
     end
     return nil, nil
@@ -242,10 +260,26 @@ local function get_number_links(entity_property)
         end
         return count
 end
-
-local function execute_immediate_class(entity_property, description, url,class_instance)
+local function isIntheDomain(entity_class,  code)
+	if not entity_class then
+		return
+	end
+        local isindomain = false
+        entity_class = entity_class:lower()
+        for k, v in pairs(mapping_class) do
+                if entity_class:find(v) then
+                        isindomain =  true
+                        break
+                end
+        end
+        if isindomain then
+                client6379:hset("domain:entity", code,1)
+        end
+end
+local function execute_immediate_class(entity_property, description, url,class_instance,code)
 		local str = nil
                 if class_instance then
+			isIntheDomain(class_instance, code)
                         client6379:hset("h_wobject_direction:"..sequence ,url,class_instance)
                         str = entity_class.get_abstract_class(code,sequence,client6379,client6380,class_instance, description)
                         if str then
@@ -260,20 +294,26 @@ local function execute_immediate_class(entity_property, description, url,class_i
 end
 local function get_immediate_class(url,code)
         local str = client6380:hget(url,"immediate_class")
+        local exist_domain = client6379:hexists("domain:entity", code)
 	local description = nil
 	local entity_property = nil
         local class_instance = nil
 
-	if not str then
+	if (not str) or (not exist_domain) then
 		description, entity_property = get_data_description(url,code)
 		local _,ci = callback.type_entity[database](entity_property)
 		class_instance =  ci
+
+	end
+	
+	if not exist_domain then
+		isIntheDomain(class_instance,code)
 	end
 
+
         if not  str then
-		
         	 if description and entity_property then
-			str = execute_immediate_class(entity_property, description, url, class_instance)
+			str = execute_immediate_class(entity_property, description, url, class_instance,code)
 	        end
 	        if str then
         	        return str
@@ -293,13 +333,14 @@ local function give_immediate_class(url,code)
 end
 
 local function set_intersection_node(node, direction)
-      if not client6379:hsetnx("h_intersection_direction:"..sequence,node..":"..direction,"") or ( node == M.start_node or node == M.end_node ) then 
-                if client6379:hsetnx("h_teste_intersection:"..sequence..":"..nivel,node,"") then 
+      if not client6379:hsetnx("h_intersection_direction:"..sequence,node..":"..direction,"") or ( node == M.start_node or node == M.end_node ) then
+                if client6379:hsetnx("h_teste_intersection:"..sequence..":"..nivel,node,"") then
 	                local page_rank = local_data.get_page_rank(node)
         	        client6379:zadd("z_wobject_direction:"..sequence..":"..nivel,page_rank,node)
 		end
       end
 end
+
 local side ={
         ["R"] = function(path,father,father_class,metapath)
                 local final = path[#path]
@@ -313,7 +354,6 @@ local side ={
 
                 if not client6379:exists(father..":"..sequence..":"..direction) then
                         if recurrent(temp,"R") then
-
                                 client6379:hset(final..":"..sequence..":"..direction,string,meta_string )
                                 set_intersection_node(final, "L")
 
@@ -330,7 +370,6 @@ local side ={
                                         value = string.." "..value
 					local temp_m = temp_meta .." "..meta
                                         meta = meta_string .." "..meta
-
                                         if recurrent(tem,"R") then
                                                 client6379:hset(final..":"..sequence..":"..direction,value,meta)
 						set_intersection_node(final, "L")
@@ -339,7 +378,7 @@ local side ={
                                 end
                         end
                 end
-        end,--expand paths --->0---->0
+        end,
 
         ["L"] = function(path,father, father_class, metapath)
                 local final = path[#path]
@@ -347,14 +386,12 @@ local side ={
                 local meta_string = table.concat(metapath, " ")
                 local temp = string
                 local temp_meta = meta_string
-                string = string..":" 
+                string = string..":"
                 meta_string = meta_string..":"
                 if not client6379:exists(father..":"..sequence..":"..direction) then
                         if recurrent(temp,"L") then
                                 client6379:hset(final..":"..sequence..":"..direction,string,meta_string )
                                 set_intersection_node(final, "R")
-
-
                         end
                 else
                         local listPaths = client6379:hgetall(father..":"..sequence..":"..direction)
@@ -393,15 +430,13 @@ local function insertObject(code,value,typev)
 	               client6380:hset(code,typev,value)
 		 end
         end
-
 end
 
 local function insertNode(node)
 	if direction then
-
 	       if client6379:hsetnx("h_wobject_direction:"..sequence..":"..nivel,node.."@"..direction,"") then
-			client6379:lpush("l_wobject_extend:"..sequence..":"..nivel,node.."@"..direction) 
-			client6379:lpush("l_wobject_remote:"..sequence..":"..nivel,node.."@"..direction)
+			client6379:lpush("l_wobject_extend:"..sequence..":"..nivel,node.."@"..direction)
+			client6379:lpush("l_wobject_remote:"..sequence..":"..nivel,node.."@"..direction) 
 		end
        end
 end
@@ -427,7 +462,7 @@ local function execute_process(node, entity_property, exists)
 
 	local temporal_description = ""
 
-        for property, values in pairs(entity_property) do --{
+        for property, values in pairs(entity_property) do
                local code_start =M.start_node
                local code_end =M.end_node
 
@@ -452,12 +487,11 @@ local function execute_process(node, entity_property, exists)
 				callback.extend[database](property, values) --get class
 			end
                 end
-	        -- action serealize
                 if xmlp.callbacks.serializedb[database] and  (not exists) then --dbpedia
 			local tmp_desc = xmlp.callbacks.serializedb[database] (values,property)
                         description = description..tmp_desc..","
                 end
-        end --}
+        end
 	if action=="extract" then
 
 		if not (temporal_description=="") then
@@ -471,80 +505,24 @@ local function execute_process(node, entity_property, exists)
 
 end
 
-local function similairy_distance (url_source,url_target ,code_source, code_target)
-	relevance.init(client6379, client6380)
-	local value = relevance.similarity_entity( url_source,url_target ,code_source, code_target, get_data_description)
-	if value and code_source and code_target then
-		client6379:zadd("list_adj_entity:"..sequence..":"..code_source,value,code_target)
-	end
-end
-
-local factor_similarity = 0
-
-local function get_adjacency_list(start)
-	factor_similarity =  0
-
-	local porcentagem  =  0.5
-
-	local ncount = client6379:zcount("list_adj_entity:"..sequence..":"..start,"-inf","+inf")
-	local size = ncount*porcentagem
-	size = math.ceil(size)
-	if size > ncount then
-		size= ncount
-	end
-	if size > 0 then
-		size = size -1
-	end
-        local list = client6379:zrevrange("list_adj_entity:"..sequence..":"..start,size, size,"withscores")
-
-        for _,values in pairs(list) do
-                local val = tonumber(values[2])
-		client6379:hset("teste_adja", val, "")
-		if val >= 0 then
-			factor_similarity = val
-		else
-			factor_similarity =  1000
-		end
-	end
-end
-local function compare_similarity(code_source ,code_target)
-        if code_source == code_target then
-                return 1
-        end
-        local key = code_source.."@"..code_target
-        if code_source > code_target then
-                key = code_target.."@"..code_source
-        end
-
-        if   client6379:hexists("teste_similarity",key) then
-                local value = client6379:hget("teste_similarity", key)
-		value = tonumber(value)
-                return value
-        else
-               return 0
-        end
-end
 
 callback ={
 	extract = { 
 		 dbpedia = function ()
                         local description =""
-                        if not client6380:hexists(url,"description") and (action=="extract")then 
-				description = local_data.get_description(start) 
+                        if not client6380:hexists(url,"description") and (action=="extract")then
+				description = local_data.get_description(start)
 				local size = nil
 				if not description then
 					local temp_url = url:gsub("http://dbpedia.org/resource/","")
 			                local n3_url =  "http://dbpedia.org/page/"..temp_url
 					temp_url = "http://dbpedia.org/page/"..url_web.escape(temp_url)
-					local  entity_property= xmlp.getStructure("dbpedia", temp_url,n3_url)
+					local  entity_property= xmlp.getStructure("dbpedia", temp_url, n3_url)
 			                if entity_property then
 		                	        description,_,size = execute_process(start,entity_property, false)
 					end
-					if not(description  == "") then
-					end
 					
 				else
-
   			                local  status, tb_description = pcall(load_description, description)
 					if status then
 	                                        local nlinks = get_number_links(tb_description)
@@ -567,9 +545,6 @@ callback ={
 
 				local  status, tb_description = pcall(load_description, description)
 				if status then
-					if action== "extend" then
-						get_adjacency_list(start) 
-					end
                         		execute_process(start, tb_description, true)
 				end
                         end
@@ -594,19 +569,13 @@ callback ={
                                 for  _,single_val in pairs(values.value) do
                                         path = {}
                                         meta_path ={}
-                                        table.insert(path, start) --get code of estart
+                                        table.insert(path, start) 
                                         table.insert(meta_path,ic_start)
 					single_val = url_web.unescape(single_val)
                                         local val, cod = filter.is[database](single_val,nil,nil)
                                         if val then
-                                                local value =	0
-                                                if not ((cod == code_start) or (cod==cod_end)) then
-							value = compare_similarity(start ,cod)
-						else
-							value = 1000
-						end
-						if value >= factor_similarity then
 							
+						if client6379:hexists("domain:entity", cod) then
         	                                	local ic =  give_immediate_class(single_val,cod)
 							if ic and #ic >0 then
                                         	                table.insert(path,code_prop)
@@ -615,7 +584,6 @@ callback ={
         	                                                table.insert(meta_path,ic)
                         	                                insertNode(cod)
         	                               	                side[direction](path,start,ic_start, meta_path)
-		        	                         else
                         	        	         end
 						end
 					end
@@ -648,11 +616,11 @@ callback ={
                                         local val, cod = filter.is[database](single_val,nil,nil)
 
                                         if val then
-						local close = true
-						if not ((cod == code_start) or (cod==cod_end)) then
-							close = local_data.distance_r(cod, code_start, code_end,M.MAX_LINKS,M.MAX_DISTANCE )
-						end
-						if close then	
+						--local close = true
+						--if not ((cod == code_start) or (cod==cod_end)) then
+						--	close = local_data.distance_r(cod, code_start, code_end,M.MAX_LINKS,M.MAX_DISTANCE )
+						--end
+						--if close then	
 		                                                local t = single_val:gsub('"','\\"')
 		                                                t = t:gsub("'","\\'")	
 								table.insert(temp_values.value, t)
@@ -661,10 +629,9 @@ callback ={
 									table.insert(cod_list, cod)
 								end
 								if client6379:hsetnx("h_wobject_immediate_class:"..sequence, start .."@"..cod,"")then 
-								 	client6379:zadd("list_adj_entity:"..sequence..":"..start,0,cod)
 								        table.insert(val_list,start.."@"..cod)
 								end
-						end
+						--end
 
                                         end
                                 end
@@ -697,7 +664,6 @@ callback ={
 				if str == nil then
 					client6380:hset(url_target,"immediate_class", "Thing,0")
 				end 
-				similairy_distance (url_source,url_target ,code_source, code_target)
 
                 end,
 
@@ -739,7 +705,7 @@ function M.generalProcess(entity)
                         start = entity
                 end
 
-                url = start
+                url = start 
 
                 if filter.code[database] then -- only dbpedia
                         url = filter.code[database](start) --get url
